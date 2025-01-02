cf6c348752274250c6fd6b5da871d17031fc1d9f
Jong MIn <unib335@naver.com>
2025-01-02 13:47:18 +0900

    conditional statements

---
+++ Conditional-Statements.playground/Contents.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+enum PoolBallType: String {
+    case solid
+    case stripe
+    case black
+}
+
+func poolBallType(forNumber number: Int) -> PoolBallType? {
+    if number < 8 && number > 0 {
+        return .solid
+    } else if number > 8 && number < 16 {
+        return .stripe
+    } else if number == 8 {
+        return .black
+    } else {
+        return nil
+    }
+}
+
+
+func printBallDetails(ofNumber number:Int) {
+    let possibleBallType = poolBallType(forNumber: number)
+    if let ballType = possibleBallType {
+        print("\(number) - \(ballType.rawValue)")
+    } else {
+        print("\(number) is not a valid pool ball number")
+    }
+}
+
+let two = poolBallType(forNumber: 2)
+let eight = poolBallType(forNumber: 8)
+let twelve = poolBallType(forNumber: 12)
+
+let zero = poolBallType(forNumber: 0)
+let sixteen = poolBallType(forNumber: 16)
+
+printBallDetails(ofNumber: 2) // 2 - solid
+printBallDetails(ofNumber: 8) // 8 - black
+printBallDetails(ofNumber: 12) // 12 - stripe
+printBallDetails(ofNumber: 0) // 0 is not a valid pool ball number
+printBallDetails(ofNumber: 16) // 16 is not a valid pool ball number
