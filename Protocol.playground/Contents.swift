abb10db65e3aadd329356b74cba6df02e4b41518
Jong MIn <unib335@naver.com>
2025-01-02 11:16:03 +0900

    protocol update

--- Protocol.playground/Contents.swift
+++ Protocol.playground/Contents.swift
@@ -55,4 +55,89 @@ class Person2: Named {
         return "Person named \(name)"
     }
 }
-                    
+
+class Friend: Person2 {
+    let age: Int
+
+    // Friend의 초기화 메서드
+    required init(name: String) {
+        self.age = 0 // 기본값으로 초기화
+        super.init(name: name)
+    }
+
+    init(name: String, age: Int) {
+        self.age = age
+        super.init(name: name)
+    }
+    
+    override func displayName() -> String {
+        return "Friend named \(name), Age: \(age)"
+    }
+}
+
+let friend = Friend(name: "Lee", age: 25)
+friend.printDescription()
+
+protocol Payable {
+    func calculateWages() -> Double
+}
+
+protocol TimeOffRequestable {
+    func requestTimeOff(days: Int) -> Bool
+}
+
+func processEmployee(employee: Payable & TimeOffRequestable) {
+    let wages = employee.calculateWages()
+    let timeOffRequest = employee.requestTimeOff(days: 10)
+}
+
+protocol Container {
+    associatedtype Item
+    mutating func add(_ item: Item)
+    var count: Int { get }
+}
+
+struct IntStack: Container {
+    typealias Item = Int
+    private var items: [Int] = []
+    
+    mutating func add(_ item: Int) { items.append(item) }
+    var count: Int { items.count }
+}
+
+func printDecription<T: Describable>(_ item: T) {
+    print(item.description)
+}
+
+protocol Runnable {
+    func run()
+}
+
+protocol Swimmable {
+    func swim()
+}
+
+protocol Flyable {
+    func fly()
+}
+
+struct Bird: Flyable, Runnable {
+    func fly() { print("Flying") }
+    func run() { print("Running") }
+}
+
+struct Fish: Swimmable {
+    func swim() { print("Swimming") }
+}
+
+struct Duck: Flyable, Runnable, Swimmable {
+    func fly() { print("Flying") }
+    func run() { print("Running") }
+    func swim() { print("Swimming") }
+}
+
+let duck = Duck()
+duck.fly()
+duck.run()
+duck.swim()
+
