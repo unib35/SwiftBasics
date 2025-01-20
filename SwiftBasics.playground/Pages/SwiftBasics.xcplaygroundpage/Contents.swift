df8d9e2fbde9016cfef1395149bddb14480edb21
Jong MIn <unib335@naver.com>
2025-01-20 10:00:42 +0900

    swift basics

---
+++ SwiftBasics.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+var greeting = "Hello, playground"
+
+
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+
+// 이것은 한 줄 주석입니다.
+
+// 이것들은 여러 개의 한 줄 주석입니다.
+// 이것은 주석입니다.
+// 안녕하세요 주석입니다.
+
+
+/*
+ 이것은 여러 줄 주석입니다.
+ 여러 줄로
+ 이어 질 수 있습니다.
+ */
+
+
+// 명명 규칙
+
+// 카멜 케이스 - iOS에서 올바른 방식
+// 첫 번째 단어는 소문자로 시작하고, 이후의 모든 단어는 첫 글자가 대문자로 시작합니다.
+let firstGreeting = "Hello, world!"
+let thisIsMyFirstGreeting = "Hello, world!"
+
+let thisismysecondgreeting = "Hello, world!"
+
+
+// 스네이크 케이스 - 잘못된 방식
+let this_is_what_snake_case_looks_like = "Hello, world!"
+
+// 파스칼 케이스 - 잘못된 방식
+let ThisIsWhatPascalCaseLooksLike = "Hello, world!"
