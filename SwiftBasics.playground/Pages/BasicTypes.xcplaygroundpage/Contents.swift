d1629ae56c006a7058b495472606f70daeb2edd1
Jong MIn <unib335@naver.com>
2025-01-23 17:19:27 +0900

    swiftuithinking basictypes

--- SwiftBasics.playground/Pages/BasicTypes.xcplaygroundpage/Contents.swift
+++ SwiftBasics.playground/Pages/BasicTypes.xcplaygroundpage/Contents.swift
@@ -1,3 +1,30 @@
 import Foundation
 
+// 문자열은 일반적인 글
+let myFirstItem: String = "Hello, world"
+
+// 이전에 생성된 객체를 참조
+let myTitle = myFirstItem
+
+// Boolean(Bool)은 참 또는 거짓
+let mySecondItem: Bool = true
+let myThirdItem: Bool = false
+
+// 잘못됨
+//let myForuthItem: String = "true"
+
+// 스위프트는 안전 타입 언어
+let myFifthItem: Bool = true
+let mySixthItem: String = "Hello world!"
+let mySeventhItem = "Hello world!"
+
+// Date
+let FirstDate: Date = Date()
+
+// 숫자는 Int, Double, CGFloat & more
+let myFirstNumber: Int = 1
+let mySecondNumber: Double = 1.0
+let myThirdNumber: CGFloat = 1.0
+
+
 
